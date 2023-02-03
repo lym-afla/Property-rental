336ba9a6773431db45bcaf289af2b5c1fc65e9d4
YL <yaroslav.linik@gmail.com>
2023-02-03 18:22:28 +0300

    Work in progress

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -8,3 +9,8 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    return render(request, "encyclopedia/entry.html", {
+        "article": entry,
+        "content": markdown2.markdown(util.get_entry(entry))
+    })
