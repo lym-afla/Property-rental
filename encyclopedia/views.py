a4ca73be3c1b95a255bc93be8462dc98c9e50916
YL <yaroslav.linik@gmail.com>
2023-02-07 23:35:29 +0300

    Adding new page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import markdown2
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django import forms
 
 from . import util
 
@@ -18,14 +19,27 @@ def entry(request, entry):
     })
     
 def search(request):
-    print(request.GET)
     if request.GET:
         search_list = []
         articles = util.list_entries()
         for line in articles:
-            if request.GET.get('q') in line:
+            if request.GET.get('q').lower() == line.lower():
+                return render(request, "encyclopedia/entry.html", {
+                    "article": line.lower(),
+                    "content": markdown2.markdown(util.get_entry(line.lower()))
+                })
+            if request.GET.get('q').upper() in line.upper():
                 search_list.append(line)
         return render(request, "encyclopedia/search.html", {
             "search": search_list
         })
     return HttpResponseRedirect(reverse("index"))
+
+class NewEntry(forms.Form):
+    title = forms.CharField(label="title")
+    content = forms.JSONField(label="content")
+
+def add_page(request):
+    return render(request, "encyclopedia/add-entry.html", {
+        "form": NewEntry()
+    })
