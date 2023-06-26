17a3d91a65a880f7075023c4f0017090e8bbc240
lym-afla <yaroslav.linik@gmail.com>
2023-06-26 16:13:14 +0300

    Initialization

--- auctions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Listing, Bid, Comment, Auction_category
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Listing)
-admin.site.register(Bid)
-admin.site.register(Comment)
-# admin.site.register(Auction_category)
