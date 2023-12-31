ad72a77614d22d646d92f3d0d56084ea4a20b1a4
lym-afla <108084946+lym-afla@users.noreply.github.com>
2023-12-31 16:48:33 +0300

    Delete property_rental/staticfiles directory

--- property_rental/staticfiles/rentals/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-    // Show property selector for the chart
-    const propertySelector = document.getElementById('chartPropertySelection').parentElement;
-    propertySelector.style.display = '';
-
-});
