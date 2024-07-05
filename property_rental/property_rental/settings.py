732b453e8d1bba094545a852bc07664f7840165d
lym-afla <108084946+lym-afla@users.noreply.github.com>
2024-07-05 12:43:47 +0300

    Update settings.py for staticfiles

--- property_rental/property_rental/settings.py
+++ property_rental/property_rental/settings.py
@@ -121,12 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Additional locations of static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'rentals', 'static', 'rentals'),
-]
+STATIC_ROOT = '/home/lymafla/property_rental/lym-afla/property_rental/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
