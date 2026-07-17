f1ab4dc8068aedb61d7b71ff991f408627b0fdae
YL-STARDESTROYER <yaroslav.linik@gmail.com>
2026-07-17 23:44:02 +0300

    fix: point asgi.py at property_rental.settings.prod (sibling of wsgi.py fix in task 1)

--- property_rental/property_rental/asgi.py
+++ property_rental/property_rental/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'property_rental.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'property_rental.settings.prod')
 
 application = get_asgi_application()
